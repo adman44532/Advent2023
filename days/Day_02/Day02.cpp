198a3eee0eb38bd50c5ccdb65369d1c667ca5e8e
adman44532 <adman44532@gmail.com>
2024-01-07 21:38:21 +1100

    Day 2 Structure

---
+++ days/Day_02/Day02.cpp
@@ -0,0 +1,9 @@
+#include "Day02.h"
+
+void Day02::part1()
+{
+}
+
+void Day02::part2()
+{
+}
---
+++ days/Day_02/Day02.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef DAY02_H
+#define DAY02_H
+
+#include "../Day.h"
+
+class Day02 : public Day
+{
+public:
+	Day02(std::string fileName) {
+		fileContents = Utils::readInput(fileName);
+	}
+	void part1() override;
+	void part2() override;
+};
+
+#endif // !DAY01_H
