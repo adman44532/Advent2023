1b3fd49cb7e0a80632a857cdd50a062e2fd083b0
adman44532 <adman44532@gmail.com>
2024-01-08 19:39:50 +1100

    Day 2 Part 1 Reworked

--- days/Day_02/Day02.cpp
+++ days/Day_02/Day02.cpp
@@ -1,11 +1,99 @@
 #include "Day02.h"
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <fstream>
+#include "../../shared/Utils.h"
+using RGB = struct
+{
+   int red;
+   int green;
+   int blue;
+};
 
 size_t Day02::part1()
 {
-   return 0;
+   std::map<std::string, int> bag = {
+      {"red", 12},
+      {"green", 13},
+      {"blue", 14}
+   };
+
+   std::vector<int> possibleGameIDs;
+
+   for ( auto& game : fileContents )
+   {
+      bool hasGameFailed = false;
+
+      // Example game line - Game 1: 12 red, 2 green, 5 blue; 9 red, 6 green, 4 blue
+      // ':' Separates the Game Info from the draws
+      // ';' Separates the different draws
+      // ' ' Separates the inbetween
+      auto gameInfo = Utils::SplitString(game, ':'); 
+
+      int gameID = std::stoi(Utils::SplitString(gameInfo[0], ' ').back());
+
+      auto gameDraws = Utils::SplitString(gameInfo[1], ';');
+
+      for ( auto& draw : gameDraws )
+      {
+         RGB tallyRGB = { 0,0,0 };
+
+         auto colours = Utils::SplitString(draw, ',');
+         for ( auto& colour : colours )
+         {
+            colour = colour.substr(1, colour.back());
+
+            auto keyValue = Utils::SplitString(colour, ' ');
+            if ( keyValue[1] == "red" )
+            {
+               tallyRGB.red += std::stoi(keyValue[0]);
+            }
+            else if ( keyValue[1] == "green" )
+            {
+               tallyRGB.green += std::stoi(keyValue[0]);
+            }
+            else if ( keyValue[1] == "blue" )
+            {
+               tallyRGB.blue += std::stoi(keyValue[0]);
+            }
+         }
+
+         if ( tallyRGB.red <= bag["red"] && tallyRGB.green <= bag["green"] && tallyRGB.blue <= bag["blue"] )
+         {
+            //std::cout << "PASSED\n\n";
+         }
+         else
+         {
+            //std::cout << "FAILED\n\n";
+            hasGameFailed = true;
+         }
+      }
+
+      if ( hasGameFailed == false )
+      {
+         possibleGameIDs.push_back(gameID);
+      }
+   }
+
+   size_t finalSum = 0;
+   for ( auto& gameID : possibleGameIDs )
+   {
+      finalSum += gameID;
+   }
+
+   return finalSum;
 }
 
 size_t Day02::part2()
 {
    return 0;
+}
+
+void Day02::display()
+{
+   std::cout << "\n--- Day 2: Cube Conundrum ---\n";
+   std::cout << "Part 1: What is the sum of the IDs of those games? " << part1() << "\n";
+   std::cout << "Part 2: What is the sum of the power of these sets? " << part2() << "\n";
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include "days/Day_01/Day01.h"
+#include "days/Day_02/Day02.h"
 #include <filesystem>
 
 int main()
 {
    Day01 day01("inputs/day01.txt");
    day01.display();
+   Day02 day02("inputs/day02.txt");
+   day02.display();
 }
